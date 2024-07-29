5dde0a31cb218b1ca80b209cb37fa762f8802dd2
pinsonanthony@gmail.com <pinsonanthony@gmail.com>
2024-07-28 23:35:23 -0400

    tuning

--- src/line_follower/line_follower/line_follower.py
+++ src/line_follower/line_follower/line_follower.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
+from rclpy.timer import Timer
 
 class LineFollower(Node):
     def __init__(self):
@@ -12,35 +13,34 @@ class LineFollower(Node):
         self.image_sub = self.create_subscription(Image, '/image_raw', self.image_callback, 10)
         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
         self.bridge = CvBridge()
-        self.linear_speed = 0.2
+        self.linear_speed = 0.5
         self.angular_speed = 0.0
+        self.publish_timer = self.create_timer(0.2, self.publish_cmd_vel)
+
 
     def image_callback(self, msg):
         frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         self.follow_line(frame)
 
     def follow_line(self, frame):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (5, 5), 0)
-        edges = cv2.Canny(blur, 100, 200)
-        contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        height, width = frame.shape[:2]
+        cropped_frame = frame[int(height * 0.5):, :]
         
-        cv2.imshow("Blurs", edges)
-        cv2.waitKey(1)
+        gray = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2GRAY)
+        _, binary = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+        M = cv2.moments(binary, False)
 
-        if contours:
-            largest_contour = max(contours, key=cv2.contourArea)
-            M = cv2.moments(largest_contour)
-            if M['m00'] > 0:
-                cx = int(M['m10'] / M['m00'])
-                cy = int(M['m01'] / M['m00'])
-                
-                cv2.circle(frame, (cx, cy), 5, (255, 0, 0), -1)
-                error = cx - frame.shape[1] // 2
-                self.angular_speed = -error * 0.002
-                self.publish_cmd_vel()
-        
-        cv2.imshow("Frame", frame)
+        if M['m00'] > 0:
+            cx = int(M['m10'] / M['m00'])
+            cy = int(M['m01'] / M['m00'])
+            cv2.circle(cropped_frame, (cx, cy), 5, (255, 0, 0), -1)
+            
+            error = cx - width / 2
+            self.angular_speed = round(-error * 0.002, 3)
+            # self.publish_cmd_vel()
+
+        cv2.imshow("binary", binary)
+        cv2.imshow("Frame", cropped_frame)
         cv2.waitKey(1)
 
     def publish_cmd_vel(self):
